88780e883dfe0d4dad3d67929b544ffec5e0baf8
John Doe <johndoe@example.com>
2022-07-22 13:44:01 +0545

    Add B.php

--- A.php
+++ A.php
@@ -1,6 +1,10 @@
 <?php
 class A{
+  private $o_className = "A";
   public function __construct(){
     echo "A's constructor called!";
+  }
+  public function getClassName(){
+    return $this->o_className;
   }
 }
---
+++ B.php
@@ -0,0 +1,6 @@
+<?php
+class B{
+  public function __construct(){
+    echo "B's constructor called!";
+  }
+}
